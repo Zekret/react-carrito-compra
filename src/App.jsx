260d25c7bb5a2b5a479c42fd35c28d21162b21c5
Zekret <benjaminconcha5@gmail.com>
2023-02-16 16:06:41 -0300

    update: filter category & price update: styles filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+import { Header } from "./components/Header";
 import Products from "./components/Products";
-import { products } from './mocks/products.json'
+import { products } from "./mocks/products.json";
 
 function App() {
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(products);
   return (
-   <Products products={products}/>
+    <>
+      <Header />
+      <Products products={filteredProducts} />
+    </>
   );
 }
 
