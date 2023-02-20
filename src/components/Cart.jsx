fa2db36850d13234d48af815f5bfbd6f956575f3
Zekret <benjaminconcha5@gmail.com>
2023-02-20 18:51:22 -0300

    update: addToCart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,36 +1,56 @@
 import './Cart.css'
-import { useId } from "react";
-import { CartIcon, ClearCartIcon } from "./Icons";
 
-export function Cart() {
-  const cartCheckboxId = useId();
+import { useId } from 'react'
+import { CartIcon, ClearCartIcon } from './Icons.jsx'
+import { useCart } from '../hooks/useCart.js'
+
+function CartItem ({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img
+        src={thumbnail}
+        alt={title}
+      />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+
+      <footer>
+        <small>
+          Qty: {quantity}
+        </small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  )
+}
+
+export function Cart () {
+  const cartCheckboxId = useId()
+  const { cart, clearCart, addToCart } = useCart()
+
   return (
     <>
-      <label className="cart-button" htmlFor={cartCheckboxId}>
+      <label className='cart-button' htmlFor={cartCheckboxId}>
         <CartIcon />
       </label>
-      <input id={cartCheckboxId} type="checkbox" hidden />
-      <aside className="cart">
+      <input id={cartCheckboxId} type='checkbox' hidden />
+
+      <aside className='cart'>
         <ul>
-            <li>
-                <img 
-                    src='https://i.dummyjson.com/data/products/2/thumbnail.jpg'
-                />
-                <div>
-                    <strong></strong>
-                </div>
-                <footer>
-                    <small>
-                        Qty1
-                    </small>
-                    <button>+</button>
-                </footer>
-            </li>
+          {cart.map(product => (
+            <CartItem
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              {...product}
+            />
+          ))}
         </ul>
-        <button>
-            <ClearCartIcon />
+
+        <button onClick={clearCart}>
+          <ClearCartIcon />
         </button>
       </aside>
     </>
-  );
+  )
 }
