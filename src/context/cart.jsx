dc41a659fcdb4e73396665a49a3168a2af970c74
Zekret <benjaminconcha5@gmail.com>
2023-02-20 19:57:05 -0300

    ipdate: reducers folder & file cart.js

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,47 +1,11 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useReducer } from "react";
+import { cartInitialState, cartReducer } from "../reducers/cart";
 
 //1. crear contexto
 export const CartContext = createContext();
 
-//Creando reducer para guardar el estado del producto, en una funcion separada
-
-const initialState = [];
-const reducer = (state, action) => {
-  const { type: actionType, payload: actionPayload } = action;
-  switch (actionType) {
-    case "ADD_TO_CART": {
-      const { id } = actionPayload;
-      const productInCartIndex = state.findIndex((item) => item.id === id);
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state);
-        newState[productInCartIndex].quantity += 1;
-        return newState;
-      }
-
-      return [
-        ...state,
-        {
-          ...actionPayload, // producto
-          quantity: 1,
-        },
-      ];
-    }
-
-    case "REMOVE_FROM_CART": {
-      const { id } = actionPayload;
-      return state.filter((item) => item.id !== id);
-    }
-
-    case "CLEAR_CART": {
-      return initialState;
-    }
-  }
-  return state;
-};
-
-//2. crear provider
-export function CartProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+function useCartReducer() {
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState);
 
   const addToCart = (product) =>
     dispatch({
@@ -56,6 +20,13 @@ export function CartProvider({ children }) {
     });
 
   const clearCart = () => dispatch({ type: "CLEAR_CART" });
+
+  return { state, addToCart, removeFromCart, clearCart };
+}
+
+//2. crear provider
+export function CartProvider({ children }) {
+  const { state, addToCart, removeFromCart, clearCart } = useCartReducer()
 
   return (
     <CartContext.Provider
