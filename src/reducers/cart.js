08f13525f62dde82f70d7d5b95062a44076d46e8
Zekret <benjaminconcha5@gmail.com>
2023-02-20 20:11:43 -0300

    update: refactor reducer

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -12,42 +12,42 @@ export const updateLocalStorage = (state) => {
   window.localStorage.setItem("cart", JSON.stringify(state));
 };
 
+const UPDATE_STATE_BY_ACTION = {
+  [CART_ACTION_TYPES.ADD_TO_CART]: (state, action) => {
+    const { id } = action.payload;
+    const productInCartIndex = state.findIndex((item) => item.id === id);
+
+    if (productInCartIndex >= 0) {
+      const newState = structuredClone(state);
+      newState[productInCartIndex].quantity += 1;
+      updateLocalStorage(newState);
+      return newState;
+    }
+
+    const newState = [
+      ...state,
+      {
+        ...action.payload, // producto
+        quantity: 1,
+      },
+    ];
+    updateLocalStorage(newState);
+    return newState;
+  },
+  [CART_ACTION_TYPES.REMOVE_FROM_CART]: (state, action) => {
+    const { id } = action.payload;
+    const newState = state.filter((item) => item.id !== id);
+    updateLocalStorage(newState);
+    return newState;
+  },
+  [CART_ACTION_TYPES.CLEAR_CART]: (state, action) => {
+    updateLocalStorage([]);
+    return [];
+  },
+};
+
 export const cartReducer = (state, action) => {
   const { type: actionType, payload: actionPayload } = action;
-  switch (actionType) {
-    case CART_ACTION_TYPES.ADD_TO_CART: {
-      const { id } = actionPayload;
-      const productInCartIndex = state.findIndex((item) => item.id === id);
-
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state);
-        newState[productInCartIndex].quantity += 1;
-        updateLocalStorage(newState);
-        return newState;
-      }
-
-      const newState = [
-        ...state,
-        {
-          ...actionPayload, // producto
-          quantity: 1,
-        },
-      ];
-      updateLocalStorage(newState);
-      return newState;
-    }
-
-    case CART_ACTION_TYPES.REMOVE_FROM_CART: {
-      const { id } = actionPayload;
-      const newState = state.filter((item) => item.id !== id);
-      updateLocalStorage(newState);
-      return newState;
-    }
-
-    case CART_ACTION_TYPES.CLEAR_CART: {
-      updateLocalStorage([]);
-      return [];
-    }
-  }
-  return state;
+  const updateState = UPDATE_STATE_BY_ACTION[actionType];
+  return updateState ? updateState(state, action) : state;
 };
